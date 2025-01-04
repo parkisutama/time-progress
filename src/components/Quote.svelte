fcbae9dba108097f63d3956d0549934225331e7d
Parkis Utama <49853923+parkisutama@users.noreply.github.com>
2025-01-04 08:18:46 +0700

    visual-restyling-test-1

---
+++ src/components/Quote.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	// Props for the quote section
+	export let quote: string =
+		'Hidupmu adalah cerminan dari pikiranmu. Jika kamu berpikir positif, maka hidupmu akan positif juga. Jika kamu berpikir negatif, maka hidupmu akan negatif juga.';
+	export let cite: string = '- Dr. Wayne Dyer';
+</script>
+
+<!-- Quote Section -->
+<blockquote class="mx-auto max-w-sm text-center italic text-gray-700 md:max-w-2xl md:pl-8">
+	<p class="py-4 text-xs font-semibold">{quote}</p>
+	<cite class="block py-2 text-center text-xs text-gray-600">{cite}</cite>
+</blockquote>
+
+<style>
+	blockquote {
+		padding-left: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,47 @@
 <script>
-    import Header from '../components/Header.svelte';
-    import TodayProgress from '../components/TodayProgress.svelte';
-    import WeekProgress from '../components/WeekProgress.svelte';
+	import Header from '../components/Header.svelte';
+	import Quote from '../components/Quote.svelte';
+	import TodayProgress from '../components/TodayProgress.svelte';
+	import WeekProgress from '../components/WeekProgress.svelte';
 	import MonthProgress from '../components/MonthProgress.svelte';
-    import QuarterProgress from '../components/QuarterProgress.svelte';
-    import YearProgress from '../components/YearProgress.svelte';
-
+	import QuarterProgress from '../components/QuarterProgress.svelte';
+	import YearProgress from '../components/YearProgress.svelte';
 </script>
 
-<main class=" dashboard container mx-auto px-4 mt-4 my-4 bg-gray-100 shadow-md">
-    <Header 
-    ></Header>
-    <section class="widget">
-        <TodayProgress  />
-    </section>
-    <section class="widget">
-        <WeekProgress />
-    <section>
-        <MonthProgress />
-    </section>
-    <section>
-        <QuarterProgress />
-    </section>
-    <section>
-        <YearProgress />
-    </section>
+<main class="dashboard mx-8 my-8 bg-gray-200 px-8 shadow-lg">
+	<div class="widget">
+		<section class="widget">
+			<Quote />
+		</section>
+		<Header></Header>
+	</div>
+	<div class="rounded-md bg-white px-2 py-8 shadow-md">
+		<section>
+			<TodayProgress />
+		</section>
+		<section>
+			<WeekProgress />
+			<section>
+				<MonthProgress />
+			</section>
+			<section>
+				<QuarterProgress />
+			</section>
+			<section>
+				<YearProgress />
+			</section>
+		</section>
+	</div>
 </main>
-<style>
-    @media (max-width: 640px) {
-    .dashboard {
-        padding: 1rem;
-    }
 
-    .widget {
-        align-items: flex-start;
-    }
-}
+<style>
+	@media (max-width: 640px) {
+		.dashboard {
+			padding: 1rem;
+		}
+
+		.widget {
+			align-items: flex-start;
+		}
+	}
 </style>
